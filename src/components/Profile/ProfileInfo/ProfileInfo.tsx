92b667ef9746ea4137432704be1775ac8f2e9e1e
Alexandra <alexbushueva27@gmail.com>
2020-10-18 16:15:30 +0300

    71 lesson

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
-import {PhotosType} from '../../../redux/profileReducer';
+import { PhotosType } from '../../../redux/profileReducer';
+import ProfileStatus from '../ProfileStatus/ProfileStatus';
 
-type PropsType ={
-  profile: {
-    userId: number
-    photos: PhotosType
-}
+type PropsType = {
+    profile: {
+        userId: number
+        photos: PhotosType
+    }
 }
 
 const ProfileInfo = (props: PropsType) => {
@@ -14,10 +15,13 @@ const ProfileInfo = (props: PropsType) => {
         <div className={s.profileInfo}>
             <img
                 src='https://images.ctfassets.net/hrltx12pl8hq/1fR5Y7KaK9puRmCDaIof7j/09e2b2b9eaf42d450aba695056793607/vector1.jpg?fit=fill&w=800&h=300'
-                alt={'purple'}/>
-            <div> Main content</div>
+                alt={'purple'} />
+
             <div className={s.ava}>
-                <img src={props.profile.photos.small} alt={'ava'}/>
+                <img src={props.profile.photos.small} alt={'ava'} />
+
+                <ProfileStatus status={"This website is so awesome!"} />
+
             </div>
         </div>
     )
